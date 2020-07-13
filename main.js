2255200ebd8858ae5f5c903ae2e7854992d2aa31
shruti mittal <shrutimittal6666@gmail.com>
2020-07-13 09:31:04 -0400

    commit

---
+++ main.js
@@ -0,0 +1,28 @@
+let request = new XMLHttpRequest();
+request.open('GET','https://Shrutimittal651.github.io/deals-lab8/products.json');
+request.responseType = 'json';
+request.send();
+
+
+request.onload = function(){
+    load(request.response);
+};
+
+function load(response)
+{
+	    let deals = response.topDeals;
+		let tag = document.getElementById('deals');
+		for (let i = 0; i < deals.length; i++) {
+        let article = document.createElement('article');
+        let title = document.createElement('h1');
+        let image = document.createElement('img');
+        let description = document.createElement('p');
+        img.setAttribute('src', 'https://Shrutimittal651.github.io/deals-lab8/images/' + deals[i].image);
+        title.textContent = deals[i].name;
+        description.textContent = 'At $' + deals[i].price + " With "+deals[i].features;
+        article.appendChild(img);
+        article.appendChild(title);
+        article.appendChild(description);
+        tag.appendChild(article);
+    }
+}
